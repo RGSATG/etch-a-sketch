e56f4716e6d8fd69f4fb4c803dab873666c849b2
RGSATG <136818066+RGSATG@users.noreply.github.com>
2024-09-23 18:02:14 +0200

    Add user input

--- logic.js
+++ logic.js
@@ -1,11 +1,20 @@
 const container = document.querySelector("#container");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
 function addDivs(containerSize, divCount) {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < divCount * divCount; i++) {
         const div = document.createElement("div");
         div.style.border = "1px solid black";
         div.style.width = div.style.height = `${containerSize / divCount - 2}px`;
         container.appendChild(div);
     }
 }
-addDivs(600, 4);
+
+button.addEventListener("click", () => {
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+    addDivs(600, input.value);
+}
+);
