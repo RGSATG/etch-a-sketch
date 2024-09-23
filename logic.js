908ae7a0306df5fb6f7ba23ac5b7341dc2ae47ee
RGSATG <136818066+RGSATG@users.noreply.github.com>
2024-09-23 21:28:51 +0200

    Limit input to above 0

--- logic.js
+++ logic.js
@@ -2,25 +2,32 @@ const container = document.querySelector("#container");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const p = document.querySelector("p");
+const containerSize = 600;
+let divCount = 0;
 
-function addDivs(containerSize, divCount) {
+function createDiv() {
+    const div = document.createElement("div");
+    div.style.border = "1px solid black";
+    div.style.width = div.style.height = `${containerSize / divCount - 2}px`;
+    return div;
+}
+
+function addDivs() {
     for (let i = 0; i < divCount * divCount; i++) {
-        const div = document.createElement("div");
-        div.style.border = "1px solid black";
-        div.style.width = div.style.height = `${containerSize / divCount - 2}px`;
-        container.appendChild(div);
+        container.appendChild(createDiv());
     }
 }
 
 button.addEventListener("click", () => {
+    divCount = input.value;
     while (container.lastElementChild) {
         container.removeChild(container.lastElementChild);
     }
-    if (input.value <= 100) {
+    if (divCount <= 100 && divCount > 0) {
         p.textContent = "The resolution of the Etch-A-Sketch can be changed here";
-        addDivs(600, input.value);
+        addDivs();
     } else {
-        p.textContent = "Please select a value which is equal to or lower than 100";
+        p.textContent = "Please select a value >= 1 and <= 100";
     }
 }
 );
