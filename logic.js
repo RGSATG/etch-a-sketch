1e6cbda472fca2dca54e06c35f8f29266a14054e
RGSATG <136818066+RGSATG@users.noreply.github.com>
2024-09-23 21:18:12 +0200

    Limit max resolution to 100 by 100

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
 const input = document.querySelector("input");
 const button = document.querySelector("button");
+const p = document.querySelector("p");
 
 function addDivs(containerSize, divCount) {
     for (let i = 0; i < divCount * divCount; i++) {
@@ -15,6 +16,11 @@ button.addEventListener("click", () => {
     while (container.lastElementChild) {
         container.removeChild(container.lastElementChild);
     }
-    addDivs(600, input.value);
+    if (input.value <= 100) {
+        p.textContent = "The resolution of the Etch-A-Sketch can be changed here";
+        addDivs(600, input.value);
+    } else {
+        p.textContent = "Please select a value which is equal to or lower than 100";
+    }
 }
 );
